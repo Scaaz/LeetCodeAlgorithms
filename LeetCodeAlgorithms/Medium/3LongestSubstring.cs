b7111528e6b353e9aea8ec762af9b6c4f8eec3f3
Scaaz <szymon.czekaj0@gmail.com>
2024-08-28 19:54:00 +0200

    Solved task 3

---
+++ LeetCodeAlgorithms/Medium/3LongestSubstring.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeAlgorithms.Medium
+{
+    /// <summary>
+    /// Task: https://leetcode.com/problems/longest-substring-without-repeating-characters
+    /// Runtime: 64ms
+    /// Memory: 42.67MB
+    /// </summary>  
+    internal class _3LongestSubstring
+    {   
+        private int LengthOfLongestSubstring(string s)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            int CurrentStreak = 0;
+            int record = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dict.TryAdd(s[i], i))
+                {
+                    CurrentStreak = 0;
+                    i = dict[s[i]];
+                    dict.Clear();
+                }
+                else
+                {
+                    CurrentStreak++;
+                    if (CurrentStreak > record)
+                    {
+                        record = CurrentStreak;
+                    }
+                }
+            }
+            return record;
+        }
+
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        [TestCase("dvdf", 3)]
+        public void TestCase(string Input, int expectedResult)
+        {
+            var result = LengthOfLongestSubstring(Input);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
