883f383cc897c59bea6969c1f423c8fc40e1183e
Scaaz <szymon.czekaj0@gmail.com>
2024-09-04 19:25:43 +0200

    Finished Task9

---
+++ LeetCodeAlgorithms/Easy/9Palindrome Number.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCodeAlgorithms.Easy
+{
+    internal class _9Palindrome_Number
+    {
+        /// <summary>
+        /// Submission: https://leetcode.com/submissions/detail/1379040406/
+        /// Runtime: 50ms
+        /// Memory: 32.79MB
+        /// </summary> 
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+            else
+            {
+                string StringInt = x.ToString();
+                for (int i = 0; i < StringInt.Length / 2; i++)
+                {
+                    if (StringInt[StringInt.Length - 1 - i] != StringInt[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        //Shortest version, but slower
+        public bool ShortestPossible(int x)
+        {
+            if (x < 0)
+                return false;
+
+            return string.Concat(x.ToString().Reverse()) == x.ToString();
+        }
+
+
+        [Test]
+        public void TestCase1()
+        {
+            var result = IsPalindrome(121);
+            var expectedResult = true;
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var result = IsPalindrome(-121);
+            var expectedResult = false;
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            var result = IsPalindrome(10);
+            var expectedResult = false;
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
