22ad294451a802f4e89acc7584bae8c28f530e0c
Scaaz <szymon.czekaj0@gmail.com>
2024-09-09 18:59:50 +0200

    Solved task 10 with regex

--- LeetCodeAlgorithms/Hard/0010RegularExpressionMatching.cs
+++ LeetCodeAlgorithms/Hard/0010RegularExpressionMatching.cs
@@ -1,56 +1,73 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Text.RegularExpressions;
 
 namespace LeetCodeAlgorithms.Hard
 {
     class _0010RegularExpressionMatching
     {
 
+        //TODO: complete without Regex
+        /// <summary>
+        /// Submission: https://leetcode.com/submissions/detail/1384481387/
+        /// Runtime: 69ms
+        /// Memory: 42.92MB
+        /// </summary>  
         public bool IsMatch(string s, string p)
         {
-            var asteriskSet = new HashSet<char>();
-            for (int i = 0; i < p.Length; i++)
-            {
-                if (p[i] == '*')
-                {
-                    asteriskSet.Add(p[i - 1]);
-                    if (p[i-1] == '.')
-                    {
-                        return true;
-                    }
-                }
-            }
-            var counter = 0;
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (p[i] == '*')
-                {
-                    counter++;
-                }
-
-                if (asteriskSet.Contains(s[i]))
-                {
-                    continue;
-                }
-
-                if (i > p.Length - 1)
-                {
-                    return false;
-                }
-
-                if (p[i] == '.')
-                {
-                    continue;
-                }
-
-
-                if (s[i] != p[i+ counter])
-                {
-                    return false;
-                }
-            }
-            return true;
-
+            return Regex.IsMatch(s, "^" + p + "$");
         }
+
+        //public bool IsMatch(string s, string p)
+        //{
+        //    var asteriskSet = new HashSet<char>();
+        //    for (int i = 0; i < p.Length; i++)
+        //    {
+        //        if (p[i] == '*')
+        //        {
+        //            asteriskSet.Add(p[i - 1]);
+        //            if (p[i-1] == '.')
+        //            {
+        //                return true;
+        //            }
+        //        }
+        //    }
+        //    var counter = 0;
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        if (p[i] == '*')
+        //        {
+        //            counter++;
+        //        }
+
+        //        if (asteriskSet.Contains(s[i]))
+        //        {
+        //            continue;
+        //        }
+
+        //        if (i > p.Length - 1)
+        //        {
+        //            return false;
+        //        }
+
+        //        if (p[i] == '.')
+        //        {
+        //            continue;
+        //        }
+
+        //        var selector = 0;
+        //        while(p[i + counter + selector] == '*' || (i + counter + 1 + selector) < p.Length && p[i + counter + 1 + selector] == '*')
+        //        {
+        //            selector++;
+        //        }
+
+        //        if (s[i] != p[i + counter])
+        //        { 
+        //            return false;
+        //        }                
+        //    }
+        //    return true;
+
+        //}
 
         [Test]
         public void TestCase()
